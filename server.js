13c47ab870057745a2dd89714bfdb96f57061be4
unknown <ebdnjf7265@kakao.com>
2025-07-22 01:21:38 +0900

    #2 git ignore 추가 및 MongoDB 연결 로직 추가

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,6 +14,10 @@ app.set('views', './views');
 
 app.use(expressLayouts);
 app.set('layout', 'layout');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use(session({
   secret: process.env.SECRET,
@@ -25,14 +31,14 @@ app.use(express.json());
 const MONGODB_URI = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}?authSource=${process.env.MONGODB_AUTH_SOURCE}`;
 
 mongoose.connect(MONGODB_URI)
-  .then(() => console.log('MongoDB 연결 성공'))
-  .catch(err => console.error('MongoDB 연결 실패:', err));
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.error(err));
 
 const indexRouter = require('./routes/index');
 const authRouter  = require('./routes/auth');
 
 app.use('/', indexRouter);
-app.use('/', authRouter);
+app.use('/auth', authRouter);
 
 app.use((err, req, res, next) => {
   const status = err.status || 500;
