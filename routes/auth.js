f8dba2d3f6e781cad77b85d6cf451bff4a2f9937
unknown <ebdnjf7265@kakao.com>
2025-07-29 00:29:39 +0900

    #3 회원가입 페이지 생성 및 연동

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user')
 
 router.get('/login', (req, res) => {
-  res.render('login');
+  res.render('login', { title: '로그인' });
 });
 
 router.post('/login', (req, res, next) => {
   const { email, password } = req.body;
-  //req.session.user = { name: 'test', email };
-  res.redirect('/');
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        return res.status(400).send('Invalid email or password');
+      }
+      if (user.password !== password) {
+        return res.status(400).send('Invalid email or password');
+      }
+
+      req.session.user = { name: user.username, email: user.email };
+      res.redirect('/');
+    })
+    .catch(err => {
+      return res.status(500).send('Server error');
+    });
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup', { title: '회원가입' });
+});
+
+router.post('/signup', (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
+
+  newUser.save()
+    .then(user => {
+      req.session.user = { name: user.username, email: user.email };
+      res.redirect('/');
+    })
+    .catch(err => {
+      return res.status(500).send('Server error');
+    });
 });
 
 router.get('/logout', (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // 로그인 여부에 따라 다른 뷰 렌더
   if (req.session.user) {
     return res.render('index', { title: '메인 페이지', user: req.session.user });
   }
